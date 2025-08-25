c44470e5641d2a07f5a63a667ac13342bb035e14
Parsa <parsashemirani@gmail.com>
2025-08-24 20:45:46 -0700

    water bucket filling idea, still brainstorming

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
 
 
 
+from datetime import datetime, timezone
 
-
-from sqlalchemy import BigInteger, String, ForeignKey
+from sqlalchemy import Integer, BigInteger, String, ForeignKey, DateTime, Date
 from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, Mapped, mapped_column
 
 class Base(MappedAsDataclass, DeclarativeBase):
@@ -12,13 +12,21 @@ class Base(MappedAsDataclass, DeclarativeBase):
 class Action(Base):
     __tablename__ = "actions"
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True, init=False)
-    name: Mapped[str] = mapped_column(String(255))
+    name: Mapped[str] = mapped_column(String())
+
+
 
 class ActionInstance(Base):
     __tablename__ = "action_instances"
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True, init=False)
     action_id: Mapped[int] = mapped_column(ForeignKey("actions.id"))
-    planned_start_time
+
+    planned_date: Mapped[Date] = mapped_column(Date)
+    planned_duration: Mapped[int | None] = mapped_column(Integer)
+    planned_start_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+    planned_end_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+
+    recorded_duration: Mapped[int | None] = mapped_column(Integer)
 
 class ActionInterval(Base):
     __tablename__ = "action_intervals"

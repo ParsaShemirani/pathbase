ed38f7d2bd43f8152c4564c7c0c5b98354a14823
Parsa <parsashemirani@gmail.com>
2025-08-26 17:39:44 -0700

    Added timeframe to models

--- models.py
+++ models.py
@@ -3,31 +3,52 @@ from datetime import datetime, timezone
 from sqlalchemy import String, ForeignKey, Integer, DateTime
 from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, Mapped, mapped_column
 
+
 class Base(MappedAsDataclass, DeclarativeBase):
     pass
 
+
 class Action(Base):
     __tablename__ = "actions"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, init=False)
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True, init=False
+    )
+
     name: Mapped[str] = mapped_column(String())
 
-class ActionPlan(Base):
-    __tablename__ = "action_plans"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, init=False)
-    action_id: Mapped[int] = mapped_column(ForeignKey("actions.id"))
-
-    duration: Mapped[int] = mapped_column(Integer)
-    deadline: Mapped[datetime] = mapped_column(DateTime(timezone=True))
 
 class ActionSegment(Base):
     __tablename__ = "action_segments"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, init=False)
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True, init=False
+    )
     action_id: Mapped[int] = mapped_column(ForeignKey("actions.id"))
 
     start_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
-    end_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), default=None)
+    end_at: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True), default=None
+    )
 
 
+class TimeFrame(Base):
+    __tablename__ = "time_frames"
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True, init=False
+    )
+
+    start_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    end_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+
+
+class ActionPlan(Base):
+    __tablename__ = "action_plans"
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True, init=False
+    )
+    time_frame_id: Mapped[int] = mapped_column(ForeignKey("time_frames.id"))
+    action_id: Mapped[int] = mapped_column(ForeignKey("actions.id"))
+
+    duration: Mapped[int] = mapped_column(Integer)
 
 
 """
--- test_scripts/stopwatch.py
+++ test_scripts/stopwatch.py
@@ -3,14 +3,24 @@ from connection import Session
 from models import Action, ActionSegment
 from sqlalchemy import select
 
+
 def initiate_unrecorded():
     unrecorded_segment = ActionSegment(
-        action_id=1,
-        start_at=datetime.now(tz=timezone.utc)
+        action_id=1, start_at=datetime.now(tz=timezone.utc)
     )
     with Session() as session:
         with session.begin():
             session.add(unrecorded_segment)
+
+
+def create_action(name: str) -> None:
+    new_action = Action(
+        name=name
+    )
+    with Session() as session:
+        with session.begin():
+            session.add(new_action)
+
 
 def switch_to_action(action_id):
     with Session() as session:
@@ -21,27 +31,27 @@ def switch_to_action(action_id):
             running_segment.end_at = datetime.now(tz=timezone.utc)
 
             new_segment = ActionSegment(
-                action_id=action_id,
-                start_at=datetime.now(tz=timezone.utc)
+                action_id=action_id, start_at=datetime.now(tz=timezone.utc)
             )
 
             session.add_all([running_segment, new_segment])
 
+
 def show_current_segment():
     with Session() as session:
         running_segment = session.scalar(
-            select(ActionSegment).where(
-                ActionSegment.end_at == None
-            )
+            select(ActionSegment).where(ActionSegment.end_at == None)
         )
         running_action = session.scalar(
             select(Action).where(Action.id == running_segment.action_id)
         )
 
-        elapsed = datetime.now(tz=timezone.utc) - running_segment.start_at.replace(tzinfo=timezone.utc)
+        elapsed = datetime.now(tz=timezone.utc) - running_segment.start_at.replace(
+            tzinfo=timezone.utc
+        )
 
         print(f"Current_time = {datetime.now()}")
 
         print(f"running_action: {running_action}")
         print(f"running_segment: {running_segment}")
-        print(f"time_elapsed: {elapsed}") 
+        print(f"time_elapsed: {elapsed}")

57835061cb8c857f3e9aa29a7577db1a5c0d601f
Parsa <parsashemirani@gmail.com>
2025-08-29 12:09:46 -0700

    finished segment logic

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -18,6 +20,7 @@ async def home(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
 
 
+# Action Manager
 @app.get("/action_manager")
 async def action_manager(request: Request):
     return templates.TemplateResponse(request=request, name="action_manager.html")
@@ -30,12 +33,11 @@ async def add_action(request: Request, name: str):
             new_action = Action(name=name)
             session.add(new_action)
         new_action_id = new_action.id
-        new_action_name = new_action.name
 
     return templates.TemplateResponse(
         request=request,
         name="display_new_action.html",
-        context={"new_action_id": new_action_id, "new_action_name": new_action_name},
+        context={"new_action_id": new_action_id, "new_action_name": name},
     )
 
 
@@ -48,12 +50,77 @@ async def all_actions(request: Request):
     )
 
 
-"""
+# Segment Manager
+@app.get("/segment_manager")
+async def segment_manager(request: Request):
+    return templates.TemplateResponse(request=request, name="segment_manager.html")
+
+
+
+
+@app.get("/start_segment")
+async def start_segment(request: Request, action_id: int):
+    with Session() as session:
+        with session.begin():
+            segment_start = datetime.now(tz=timezone.utc)
+            new_segment = ActionSegment(
+                action_id=action_id,
+                start_at=segment_start,
+            )
+            session.add(new_segment)
+        segment_action_name = new_segment.action.name
+        segment_id = new_segment.id
+    return templates.TemplateResponse(
+        request=request,
+        name="display_started_segment.html",
+        context={
+            "segment_action_name": segment_action_name,
+            "segment_start": segment_start,
+            "segment_id": segment_id,
+        },
+    )
+
+@app.get("/running_segment_info")
+async def running_segment_info(request: Request):
+    with Session() as session:
+        running_segment = session.scalar(
+            select(ActionSegment).where(ActionSegment.end_at == None)
+        )
+        current_time = datetime.now(tz=timezone.utc)
+        segment_start = running_segment.start_at
+        segment_action_name = running_segment.action.name
+        segment_duration = current_time - segment_start.replace(tzinfo=timezone.utc)
+    return templates.TemplateResponse(
+        request=request,
+        name="running_segment_info.html",
+        context={
+            "segment_action_name": segment_action_name,
+            "segment_duration": segment_duration,
+            "segment_start": segment_start,
+            "current_time": current_time,
+        }
+    )
+
+
 @app.get("/end_segment")
 async def end_segment(request: Request):
     with Session() as session:
         with session.begin():
-            ended_segment_dict = end_running_segment(session=session)
-        session.refresh(ended_segment)
-    return templates.TemplateResponse(request=request, name="display_ended_segment.html", context={"ended_segment": ended_segment})
-    """
+            running_segment = session.scalar(
+                select(ActionSegment).where(ActionSegment.end_at == None)
+            )
+            current_time = datetime.now(tz=timezone.utc)
+            segment_start = running_segment.start_at
+            segment_action_name = running_segment.action.name
+            running_segment.end_at = current_time
+            segment_duration = current_time - segment_start.replace(tzinfo=timezone.utc)
+    return templates.TemplateResponse(
+        request=request,
+        name="display_ended_segment.html",
+        context={
+            "segment_start": segment_start,
+            "segment_end": current_time,
+            "segment_action_name": segment_action_name,
+            "segment_duration": segment_duration,
+        },
+    )

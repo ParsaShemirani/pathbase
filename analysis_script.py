5361fec2f32828ff7f6498391948472d472a16d1
Parsa Shemirani <parsashemirani@gmail.com>
2025-10-09 15:31:45 -0700

    Added duration stats in the output, variables for start and end times fo the timeframe.

--- analysis_script.py
+++ analysis_script.py
@@ -13,6 +13,14 @@ from dotenv import load_dotenv
 from connection import Session
 from models import ActionSegment
 
+### Modify these variables:
+local_zoneinfo = ZoneInfo("America/Los_Angeles")
+
+local_timeframe_start = datetime(year=2025, month=10, day=8, hour=0, tzinfo=local_zoneinfo)
+local_timeframe_end = datetime(year=2025, month=10, day=9, hour=0, tzinfo=local_zoneinfo)
+
+###
+
 load_dotenv()
 ANALYSIS_OUTPUT_DIRECTORY = os.getenv("ANALYSIS_OUTPUT_DIRECTORY")
 if ANALYSIS_OUTPUT_DIRECTORY is None:
@@ -28,30 +36,46 @@ class Display_Row:
     duration: str
 
 display_rows: list[Display_Row] = []
+timeframe_recorded_duration = timedelta()
+
+def format_hms(td: timedelta) -> str:
+    total = int(td.total_seconds())
+    h, r = divmod(total, 3600)
+    m, s = divmod(r, 60)
+    return f"{h:02}:{m:02}:{s:02}"
 
 with Session() as session:
     rows = session.scalars(
         select(ActionSegment)
     ).all()
-  
+
+    if rows[-1].end_at is None:
+        del rows[-1]
+
+    timeframe_start_at = rows[0].start_at
+    timeframe_end_at = rows[-1].end_at
+
     for row in rows:
         naive_utc_start_at = row.start_at
         naive_utc_end_at = row.end_at
 
         aware_utc_start_at = naive_utc_start_at.replace(tzinfo=timezone.utc)
-        la_start_at = aware_utc_start_at.astimezone(tz=ZoneInfo("America/Los_Angeles"))
-        start_at_str = la_start_at.strftime("%Y-%m-%d %H:%M:%S")
+        aware_utc_end_at = naive_utc_end_at.replace(tzinfo=timezone.utc)
 
-        duration_str = ""
+        local_start_at = aware_utc_start_at.astimezone(tz=ZoneInfo("America/Los_Angeles"))
+        local_end_at = aware_utc_end_at.astimezone(tz=ZoneInfo("America/Los_Angeles"))
 
-        if naive_utc_end_at is None:
-            la_end_at = None
-        else:
-            aware_utc_end_at = naive_utc_end_at.replace(tzinfo=timezone.utc)
-            la_end_at = aware_utc_end_at.astimezone(tz=ZoneInfo("America/Los_Angeles"))
-            end_at_str = la_end_at.strftime("%Y-%m-%d %H:%M:%S")
-            duration = aware_utc_end_at - aware_utc_start_at
-            duration_str = str(duration)
+        if local_start_at < local_timeframe_start and local_end_at < local_timeframe_start:
+            continue
+        if local_end_at > local_timeframe_end and local_start_at > local_timeframe_end:
+            continue
+
+        start_at_str = local_start_at.strftime("%Y-%m-%dT%H:%M:%S")
+        end_at_str = local_end_at.strftime("%Y-%m-%dT%H:%M:%S")
+        
+        duration = aware_utc_end_at - aware_utc_start_at
+        duration_str = str(duration)
+        timeframe_recorded_duration += duration
         
         display_row = Display_Row(
             id=row.id,
@@ -83,3 +107,21 @@ with open(output_file_path, 'w', newline='') as csvfile:
     for dr in display_rows:
         writer.writerow(rowdict=asdict(dr))
 
+timeframe_total_duration = timeframe_end_at - timeframe_start_at
+timeframe_unrecorded_duration = timeframe_total_duration - timeframe_recorded_duration
+timeframe_percentage_recorded = 100 * (timeframe_recorded_duration.total_seconds() / timeframe_total_duration.total_seconds())
+timeframe_percentage_unrecorded = 100 * (timeframe_unrecorded_duration.total_seconds() / timeframe_total_duration.total_seconds())
+
+recorded_stats = ""
+recorded_stats += f"""\
+Timeframe total duration: {format_hms(td=timeframe_total_duration)}
+Timeframe recorded duration: {format_hms(td=timeframe_recorded_duration)}
+Timeframe unrecorded duration: {format_hms(td=timeframe_unrecorded_duration)}
+
+Timeframe recorded percentage: {timeframe_percentage_recorded}%
+Timeframe unrecorded percentage: {timeframe_percentage_unrecorded}%
+"""
+
+with open(output_file_path, "a") as f:
+    f.write(3*"\n")
+    f.write(recorded_stats)
